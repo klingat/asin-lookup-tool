1a0475942005aebf15322fb6c397b3b665c46ea3
Katarina Lingat <katarinalingat@Katarinas-MacBook-Pro.local>
2019-08-06 22:01:09 -0700

    Create seed file for testing purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+products = Product.create([
+  { asin: "B0013BKDO8", category: "Tools & Home Improvement", rank: 92, dimensions: "1.3 x 2.5 x 3 inches" },
+  { asin: "B07L6VQDSC", category: "Toys & Games", rank: 30, dimensions: "1.3 x 2.5 x 3 inches" },
+  { asin: "B002QYW8LW", category: "Baby", rank: 36, dimensions: "1.3 x 2.5 x 3 inches" }
+])
