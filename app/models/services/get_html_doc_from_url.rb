4193b2c49bd6dff749485e3cb47b8588b9ce76c2
Katarina Lingat <katarinalingat@Katarinas-MacBook-Pro.local>
2019-08-12 21:49:38 -0700

    Remove comments

--- app/models/services/get_html_doc_from_url.rb
+++ app/models/services/get_html_doc_from_url.rb
@@ -10,27 +10,12 @@ class Services::GetHtmlDocFromUrl
   end
 
   def doc
-    # binding.pry
     response = HTTParty.get(url, {
       headers: {
-        "User-Agent" => "Httparty",
-        "Accept" => "text/html"
+        "User-Agent" => "Httparty"
       }
     })
     Nokogiri::HTML(response)
   end
 
 end
-
-# response = HTTParty.get('https://www.amazon.com/dp/B0013BKDO8', {
-#   headers: {"User-Agent" => "Httparty"}
-# })
-
-# doc = Nokogiri::HTML.parse(response)
-
-# doc.css('#prodDetails table > tr:nth-child(3) td:first.a-size-base')
-
-# //*[@id="prodDetails"]/div[2]/div[1]/div/div[2]/div/div/table/tbody/tr[2]/td[2]
-# # response = HTTParty.get("www.amazon.com/dp/B0013BKDO8", {
-# #   headers: {"User-Agent" => "Httparty"}
-# # })
